d8079966ac6efa3c80523025614033c21e2611c8
emerald73 <>
2020-12-24 10:24:35 -0500

    removed typing from uptime

--- boblin/Cogs/uptime.py
+++ boblin/Cogs/uptime.py
@@ -13,31 +13,29 @@ class uptime(commands.Cog):
 
     @commands.command(name="uptime", aliases=['up'])
     async def get_uptime(self, ctx):
-        async with ctx.typing():
-            await asyncio.sleep(1)
-            p = arrow.utcnow()
-            diff = (p - self.g)
-            days = diff.days
-            hours = int(diff.seconds / 3600)
-            minutes = int(diff.seconds / 60) % 60
-            if days == 1:
-                dd = "day"
-            else:
-                dd = "days"
-            if hours == 1:
-                hh = "hour"
-            else:
-                hh = "hours"
-            if minutes == 1:
-                mm = "minute"
-            else:
-                mm = "minutes"
-            embed = discord.Embed(color=discord.Color.green(),
+        p = arrow.utcnow()
+        diff = (p - self.g)
+        days = diff.days
+        hours = int(diff.seconds / 3600)
+        minutes = int(diff.seconds / 60) % 60
+        if days == 1:
+            dd = "day"
+        else:
+            dd = "days"
+        if hours == 1:
+            hh = "hour"
+        else:
+            hh = "hours"
+        if minutes == 1:
+            mm = "minute"
+        else:
+            mm = "minutes"
+        embed = discord.Embed(color=discord.Color.green(),
                                                description="Boblin has been online for " + str(days) + " " + dd + ", " + str(
                                                    hours) + " " + hh + ", and " + str(minutes) + " " + mm + ".")
-            embed.set_author(name="Uptime")
-            embed.set_footer(text="Boblin the Goblin#4756")
-            await ctx.send(embed=embed)
+        embed.set_author(name="Uptime")
+        embed.set_footer(text="Boblin the Goblin#4756")
+        await ctx.send(embed=embed)
 
 def setup(bot):
     bot.add_cog(uptime(bot))
